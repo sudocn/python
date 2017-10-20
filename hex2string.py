6ab161509f5e3ab3ab8d570d8baa3f621cb0fd9b
cpeng <pengcheng.cn@gmail.com>
2017-10-20 10:13:33 +0800

    print format changes in hex2string.py

--- hex2string.py
+++ hex2string.py
@@ -3,15 +3,17 @@
 import sys
 
 def hex2string(hex_string):
+    output=''
     if len(hex_string) % 2 == 1:
         hex_string = '0' + hex_string
 
     while len(hex_string) > 0:
         char,hex_string = hex_string[:2], hex_string[2:]
-        print chr(int(char, 16)),
+        output += chr(int(char, 16))
+    return output
 
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
         print "usage: " + sys.argv[0] + " hex_stirng"
     else:
-        hex2string(sys.argv[1])
+        print hex2string(sys.argv[1])
